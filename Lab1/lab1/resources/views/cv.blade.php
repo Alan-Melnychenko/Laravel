c4b62f84ee31255353851dfbadcda19ec6724fae
Alan-Melnychenko <93719770+Alan-Melnychenko@users.noreply.github.com>
2023-04-04 00:17:52 +0300

    lab

---
+++ Lab1/lab1/resources/views/cv.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>CV</title>
+    </head>
+    <body>
+        <h1>Мельниченко Алан</h1>
+        <p>Junior Laravel Developer</p>
+        <h2>Навички</h2>
+        <ul>
+            <li>Laravel</li>
+            <li>PHP</li>
+            <li>MySQL</li>
+            <li>HTML/CSS</li>
+        </ul>
+        <h2>Досвід роботи</h2>
+        <p>Немає досвіду роботи на посаді Laravel Developer</p>
+        <h2>Освіта</h2>
+        <p>Ужгородський Національний Університет, Факультет математики та цифрових технологій</p>
+        <h2>Контакти</h2>
+        <ul>
+            <li>Email: melnychenko.alan@uzhnu.edu.ua</li>
+            <li>Телефон: +380958788799</li>
+        </ul>
+    </body>
+</html>
